638e7d3aa4d944e29e58bcaa4795176cdf20fa04
Kenneth Kobina Essien Aidoo <essienkent@gmail.com>
2026-04-26 22:33:20 +0000

    whosh

--- ZombieFactory.sol
+++ ZombieFactory.sol
@@ -31,3 +31,34 @@ function _createZombies(string memory _name, uint _dna) private{
      //   uint rand = uint(keccak256(abi.encodePacked(_str)));
       //  return rand % dnaModulus;
 }
+
+pragma solidity >= 0.5.0 <0.6.0;
+
+
+contract  ZombieFactory{
+   //uint --unsigned integers= positive values
+   // int === signed integers = negative values 
+   // uint = uint256
+   // different categories of uint..uint8 uint32 uint256
+   
+
+uint dnaDigits = 16;
+uint dnaModulus = 10 ** dnaDigits;
+
+struct Zombie {
+       string name;
+       uint dna;
+}
+
+Zombie[] public zombie;
+
+function _createZombies(string memory _name, uint _dna) private{
+        zombies.push(Zombies[_name,_dna]);
+
+}
+
+function _generateZombies(sttring memeory _str) private view returns(uint){
+        uint rand = uint(keccak256(abi.encodePacked(_str)));
+        return rand % dnaModulus;
+}
+
